e15f95479f1c126273332964eaffabb612a22c51
hoyoung <a01066651676@gmail.com>
2026-05-25 23:51:53 +0900

    [INF] 8.응급실

---
+++ src/Inflearn_study/stack/Main8.java
@@ -0,0 +1,16 @@
+package Inflearn_study.stack;
+
+import java.io.*;
+import java.util.*;
+public class Main8 {
+    public int solution (String str , String pats) {
+
+    }
+    public static void main (String[] args) throws Exception {
+        Main8 m = new Main8();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        String pats = br.readLine();
+        System.out.println(m.solution(str,pats));
+    }
+}
