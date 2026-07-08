e680d6ce0a3196152aada163446590d9b35f67d0
parkhoyoung <a01066651676@gmail.com>
2026-07-08 10:32:18 +0900

    [INF]10.미로탐색(DFS)

---
+++ src/Inflearn_study/dfs/Main10.java
@@ -0,0 +1,45 @@
+package Inflearn_study.dfs;
+import java.util.*;
+import java.io.*;
+
+public class Main10 {
+    static int[][] board;
+    static boolean[][] visited = new boolean[7][7];
+    static int[] xArrays = {-1,0,1,0};
+    static int[] yArrays = {0,1,0,-1};
+
+    static int cnt = 0;
+
+    public void dfs(int x  , int y) {
+        if(x < 0 || x > 6 || y < 0 || y > 6) return;
+
+        if(board[x][y] == 1 | visited[x][y]) return;
+
+        if(x == 6 && y == 6) {
+            cnt++;
+            return;
+        }
+        visited[x][y] = true;
+
+        for(int i = 0 ; i < 4 ; i++) {
+            dfs(x+xArrays[i],y+yArrays[i]);
+        }
+
+        visited[x][y] = false;
+
+    }
+    public static void main(String[] args) throws Exception {
+        Main10 m = new Main10();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        board = new int[7][7];
+
+        for ( int i = 0 ; i < 7 ; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int j = 0 ; j < 7 ; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        m.dfs(0,0);
+        System.out.println(cnt);
+    }
+}
