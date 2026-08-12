2b10584ad4a51b5c4ecbfbabb0a1d3b6a9e68afc
parkhoyoung <a01066651676@gmail.com>
2026-08-12 11:00:32 +0900

    [INF] refactoring

---
+++ src/refactoring/Main1.java
@@ -0,0 +1,25 @@
+package refactoring;
+import java.util.*;
+import java.io.*;
+
+public class Main1 {
+    public int solution (String first , char c) {
+        int result = 0;
+
+        c = Character.toUpperCase(c);
+        for(char ch : first.toCharArray()) {
+            if(Character.toUpperCase(ch) == c) {
+                result++;
+            }
+        }
+        return result;
+    }
+    public static void main(String args[])throws Exception {
+        Main1 m = new Main1();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String first = br.readLine();
+        char c = br.readLine().charAt(0);
+
+        System.out.println(m.solution(first , c));
+    }
+}
