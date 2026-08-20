ed80b36edca1ca8bca465a6debd013bdb06d95f5
parkhoyoung <a01066651676@gmail.com>
2026-08-20 09:49:57 +0900

    [INF] refactoring

---
+++ src/refactoring/Main12.java
@@ -0,0 +1,26 @@
+package refactoring;
+import java.io.*;
+import java.util.*;
+
+public class Main12 {
+    public String solution (int size , String str) {
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0 ; i < size ; i++) {
+            char ascii = (char)Integer.parseInt(str.substring(i*7 , i*7+7)
+                    .replace("#","1").replace("*","0")
+                    ,2);
+            sb.append(ascii);
+        }
+
+        return sb.toString();
+    }
+    public static void main(String[] args) throws Exception{
+        Main12 m = new Main12();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int size = Integer.parseInt(br.readLine());
+        String str = br.readLine();
+
+        System.out.println(m.solution(size , str));
+    }
+}
